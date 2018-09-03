7aaf4ae04501369d5a171efa0adc39a8e441fe5a
Debasish Nandi <debasish17.wgt@gmail.com>
2018-09-03 16:08:07 +0530

    initial commit - game play activity design done

---
+++ app/src/main/java/com/wgt/tictactoe/view/GamePlayActivity.java
@@ -0,0 +1,15 @@
+package com.wgt.tictactoe.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wgt.tictactoe.R;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_play);
+    }
+}
